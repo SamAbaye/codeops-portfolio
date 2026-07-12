0f9c0cf8a1af1dfe30b7e01d881bea3fa808d157
SamAbaye <samitube30@gmail.com>
2026-07-12 19:08:18 +0300

    Added comment to to each excercise to make it more nit

--- Module-01/day02-IntroToPython/practice.py
+++ Module-01/day02-IntroToPython/practice.py
@@ -1,3 +1,4 @@
+#Excercise 1 - Temperature label
 temp = int(input('input the temprature in celcius '))
 
 if temp < 15:
@@ -7,18 +8,22 @@ elif temp > 15 and temp < 28:
 else:
     print("Hot")
 
+#Excercise 2 -  Receipt loop
 for i in range(10):
     print(f'Receipt #{i}')
 
+#Excercise 3 - Even numbers
 for i in range(20):
     if i % 2 == 0:
         print(i)
 
+#Excercise 4 -  Discount function
 def apply_discount(price, percent=10):
     return price - (price * 1/percent)
 
 print(apply_discount(250))
 
+#Excercise 5 - Countdown
 i = 5
 while i > 0:
     print(i)
